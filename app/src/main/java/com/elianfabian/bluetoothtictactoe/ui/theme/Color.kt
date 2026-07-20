4d21b3ce8659f8a661a389f56ccbfed743d4db3f
Elián Fabián <elian.fabian.2001@gmail.com>
2026-07-20 13:12:18 +0200

    Update app theme to blue color palette and avoid using dynamic colors

--- app/src/main/java/com/elianfabian/bluetoothtictactoe/ui/theme/Color.kt
+++ app/src/main/java/com/elianfabian/bluetoothtictactoe/ui/theme/Color.kt
@@ -2,10 +2,34 @@ package com.elianfabian.bluetoothtictactoe.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme Blue Palette
+val Blue40 = Color(0xFF005DB7)
+val Blue90 = Color(0xFFD7E2FF)
+val Blue10 = Color(0xFF001A40)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlueGrey40 = Color(0xFF565E71)
+val BlueGrey90 = Color(0xFFDAE2F9)
+val BlueGrey10 = Color(0xFF131C2C)
+
+val LightBlue40 = Color(0xFF4D606E)
+val LightBlue90 = Color(0xFFD0E4F2)
+val LightBlue10 = Color(0xFF081D29)
+
+// Dark Theme Blue Palette
+val Blue80 = Color(0xFFABC7FF)
+val Blue20 = Color(0xFF002F65)
+val Blue30 = Color(0xFF00458C)
+
+val BlueGrey80 = Color(0xFFBEC6DC)
+val BlueGrey20 = Color(0xFF283141)
+val BlueGrey30 = Color(0xFF3E4759)
+
+val LightBlue80 = Color(0xFFB4C8D8)
+val LightBlue20 = Color(0xFF1F323E)
+val LightBlue30 = Color(0xFF354955)
+
+// Surfaces
+val BlueGrey95 = Color(0xFFF1F0F4)
+val BlueGrey99 = Color(0xFFFEFBFF)
+val BlueGrey25 = Color(0xFF33353A)
+val BlueGrey15 = Color(0xFF1B1B1F)
